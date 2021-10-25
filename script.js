e0eae46e99996606295f5dbc480fdb16986abbf8
samihanowrin123 <nowrinorpi001@gmail.com>
2021-10-25 14:18:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.ready(function(){
+//     window.scroll(function(){
+//         if(this.scroll > 20){
+//             $('.navbar').addClass("sticky");
+
+//        }else{
+//            $('.navbar').removeClass("sticky");
+//     }
+// });
+// $('.menu-btn').click(function(){
+//     $('.navbar .menu').toggleClass("active");
+// });
+// });
+
+(function(){
+    console.log("test");
+})();
